e39b6fcb7b05557dc8bf800b50d15169c0d9e23f
arnold5sweet <arnold.kmulera@gmail.com>
2017-10-03 14:39:50 +0300

    Global API

---
+++ app/src/main/java/com/example/user/moviesapp/network/Globals.java
@@ -0,0 +1,9 @@
+package com.example.user.moviesapp.network;
+
+/**
+ * API_KEY request for my database
+ */
+
+public class Globals {
+    public static String API_KEY = "eca9d3cc37160a9dc769b6bdcd0e1eed";
+}
